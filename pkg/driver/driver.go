3cf05202d7b132dd9403203979b6c93366250c0e
RahmaYasser <rahma.y9079@gmail.com>
2022-02-09 14:17:44 +0200

    project structure and db connection

---
+++ pkg/driver/driver.go
@@ -0,0 +1,36 @@
+package driver
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func GetDbConnetion() *sql.DB {
+	dataSourceName := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"))
+	db, err := sql.Open("pgx", dataSourceName)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("unable to conect to db"))
+		panic(err)
+	}
+	log.Println("connected to db ")
+
+	//test connection
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal("cannot ping db")
+		panic(err)
+	}
+	log.Println("pinged db")
+	return db
+}
+
