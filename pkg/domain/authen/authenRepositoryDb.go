1549091c431e823829e9ec6349bd4072f07d2938
RahmaYasser <rahma.y9079@gmail.com>
2022-02-10 14:10:46 +0200

    user crud

--- pkg/domain/authen/authenRepositoryDb.go
+++ pkg/domain/authen/authenRepositoryDb.go
@@ -3,7 +3,6 @@ package authen
 import (
 	"database/sql"
 	"fmt"
-
 	"github.com/spear-app/spear-go/pkg/domain/user"
 )
 
@@ -22,6 +21,51 @@ func (r AuthenRepositoryDb) Signup(user *user.User) error {
 func (r AuthenRepositoryDb) Login(user *user.User) error {
 	row := r.db.QueryRow(`SELECT id, name, email, password, gender FROM users WHERE email=$1`, user.Email)
 	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.Gender)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Update only name and gender could be updated
+func (r AuthenRepositoryDb) Update(user *user.User, id string) error {
+	var name string
+	row := r.db.QueryRow(`SELECT name FROM users WHERE id= $1`, id)
+	err := row.Scan(&name)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	_, err = r.db.Exec(`UPDATE users SET name=$1, gender=$2 WHERE id=$3`,
+		user.Name, user.Gender, id)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+//Delete This function to delete a user/talent
+func (r AuthenRepositoryDb) Delete(id string) error {
+	var usrType string
+	row := r.db.QueryRow(`SELECT first_name FROM users WHERE id= $1`, id)
+	err := row.Scan(&usrType)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	_, err = r.db.Exec(`DELETE FROM users WHERE id=$1`, id)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (r AuthenRepositoryDb) ReadUserByID(id string, user *user.User) error {
+	row := r.db.QueryRow(`SELECT id, name, email, gender, created_at, updated_at, deleted_at FROM users WHERE id=$1`,
+		id)
+	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Gender, &user.CreatedAt, &user.UpdatedAt, &user.DeletedAt)
 	if err != nil {
 		return err
 	}
