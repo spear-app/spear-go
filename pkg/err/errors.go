e6d509e5335b164627bb252adf681f55631b189b
RahmaYasser <rahma.y9079@gmail.com>
2022-02-26 16:04:17 +0200

    added new error types

--- pkg/err/errors.go
+++ pkg/err/errors.go
@@ -3,18 +3,20 @@ package errs
 import "errors"
 
 var (
-	ErrDb              = errors.New("unexpected database error")
-	ErrNoRowsFound     = errors.New("no values found")
-	ErrServerErr       = errors.New("internal server error")
-	ErrInvalidPassword = errors.New("invalid password")
-	ErrMissingPassword = errors.New("missing password")
-	ErrMissingName     = errors.New("missing name")
-	ErrMissingGender   = errors.New("missing gender")
-	ErrInvalidToken    = errors.New("invalid token")
-	ErrDuplicateValue  = errors.New("this value already exists")
-	ErrEmailMissing    = errors.New("email is missing")
-	ErrInvalidEmail    = errors.New("invalid email")
-	ErrInvalidName     = errors.New("invalid name")
+	ErrDb                      = errors.New("unexpected database error")
+	ErrNoRowsFound             = errors.New("no values found")
+	ErrServerErr               = errors.New("internal server error")
+	ErrInvalidPassword         = errors.New("invalid password")
+	ErrMissingPassword         = errors.New("missing password")
+	ErrMissingName             = errors.New("missing name")
+	ErrMissingGender           = errors.New("missing gender")
+	ErrInvalidToken            = errors.New("invalid token")
+	ErrDuplicateValue          = errors.New("this value already exists")
+	ErrEmailMissing            = errors.New("email is missing")
+	ErrInvalidEmail            = errors.New("invalid email")
+	ErrInvalidName             = errors.New("invalid name")
+	ErrContentParams           = errors.New("parameter value is not valid")
+	ErrInvalidVerificationCode = errors.New("invalid verification code")
 )
 
 type Response struct {
