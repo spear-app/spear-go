18d608234a2fbd1595d36380465437bc233a7b1c
RahmaYasser <rahma.y9079@gmail.com>
2022-02-26 16:25:46 +0200

    finished email verfication queries

--- pkg/domain/authen/authenRepositoryDb.go
+++ pkg/domain/authen/authenRepositoryDb.go
@@ -19,14 +19,49 @@ func (r AuthenRepositoryDb) Signup(user *user.User) error {
 	return nil
 }
 
-func (r AuthenRepositoryDb) InsertOTP(user user.User) error {
-	err := r.db.QueryRow(`INSERT INTO users(otp) VALUES ($1)RETURNING id;`, user.OTP).Scan(&user.ID)
+func (r AuthenRepositoryDb) InsertOTP(user *user.User) error {
+	var name string
+	row := r.db.QueryRow(`SELECT name FROM users WHERE id= $1`, user.ID)
+	err := row.Scan(&name)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	_, err = r.db.Exec(`UPDATE users SET otp=$1 WHERE id=$2`,
+		user.OTP, user.ID)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	return nil
 }
+
+func (r AuthenRepositoryDb) ReadOTP(user *user.User) error {
+	row := r.db.QueryRow(`SELECT otp FROM users WHERE id=$1`,
+		user.ID)
+	err := row.Scan(&user.OTP)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r AuthenRepositoryDb) VerifyEmail(user *user.User) error {
+	var name string
+	row := r.db.QueryRow(`SELECT name FROM users WHERE id= $1`, user.ID)
+	err := row.Scan(&name)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	_, err = r.db.Exec(`UPDATE users SET email_verified=true WHERE id=$1`, user.ID)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
 func (r AuthenRepositoryDb) Login(user *user.User) error {
 	row := r.db.QueryRow(`SELECT id, name, email, password, gender FROM users WHERE email=$1`, user.Email)
 	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.Gender)
@@ -38,16 +73,6 @@ func (r AuthenRepositoryDb) Login(user *user.User) error {
 
 // ReadUserByID obviously to read by id
 func (r AuthenRepositoryDb) ReadUserByID(user *user.User) error {
-	row := r.db.QueryRow(`SELECT id, name, email, gender, created_at, updated_at, deleted_at FROM users WHERE id=$1`,
-		user.ID)
-	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Gender, &user.CreatedAt, &user.UpdatedAt, &user.DeletedAt)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (r AuthenRepositoryDb) ReadOTP(user *user.User) error {
 	row := r.db.QueryRow(`SELECT id, name, email, gender, created_at, updated_at, deleted_at FROM users WHERE id=$1`,
 		user.ID)
 	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Gender, &user.CreatedAt, &user.UpdatedAt, &user.DeletedAt)
