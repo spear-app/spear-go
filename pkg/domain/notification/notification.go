8ca1a0ce549925dc5b00f5a6141b011cc8cdd89c
RahmaYasser <rahma.y9079@gmail.com>
2022-04-02 16:06:37 +0200

    notification type added

---
+++ pkg/domain/notification/notification.go
@@ -0,0 +1,13 @@
+package notification
+
+import "time"
+
+type Notification struct{
+	ID				uint 		`json:"id"`
+	UserUID			uint  		`json:"user_id"`
+	CreatedAt		time.Time  	`json:"created_at"`
+	UpdatedAt		time.Time 	`json:"updated_at"`
+	DeletedAt		*time.Time	`json:"deleted_at"`
+	Title			string		`json:"title"`
+	Body			string		`json:"body"`
+}
