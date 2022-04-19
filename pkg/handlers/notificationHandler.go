fbf3ee8e1c8f2f14c9cef49ca65fa685606144e3
RahmaYasser <rahma.y9079@gmail.com>
2022-04-19 07:27:04 +0200

    create notification handler, decoding params

---
+++ pkg/handlers/notificationHandler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/spear-app/spear-go/pkg/domain/notification"
+	errs "github.com/spear-app/spear-go/pkg/err"
+	"github.com/spear-app/spear-go/pkg/service"
+)
+
+type NotificationHandlers struct {
+	service service.NotificationService
+}
+
+
+func (authenHandler AuthenHandlers) Create(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	var notiObj notification.Notification
+	json.NewDecoder(r.Body).Decode(&notiObj)
+	var userIdStr string = strconv.FormatUint(uint64(notiObj.UserUID), 10)
+	_, err := strconv.Atoi(userIdStr)
+	if err!=nil{
+		//TODO response with bad request 400
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errs.NewResponse("invalid user id", http.StatusBadRequest))
+		return
+	}
+	
+}
