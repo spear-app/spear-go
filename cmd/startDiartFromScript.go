521e3cdd79fe595a2b8408c4c1212da757c60fe6
RahmaYasser <rahma.y9079@gmail.com>
2022-07-06 23:31:33 +0200

    catching stdout once it's filled

--- cmd/startDiartFromScript.go
+++ cmd/startDiartFromScript.go
@@ -11,16 +11,53 @@ func startConv() *exec.Cmd {
 	fmt.Println("starting conversation .........")
 	cmd := exec.Command("bash", "-c", "source "+"/home/rahma/spear-go/pkg/scripts/diart_run4.sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	/*var outb, errb bytes.Buffer
+	errb.Reset()
+	outb.Reset()
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb*/
+	stdout, err := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Println("sleeping-----------")
+	for {
+		tmp := make([]byte, 1024)
+		_, err := stdout.Read(tmp)
+		if err != nil {
+			fmt.Println(err.Error())
+			break
+		}
+		str := string(tmp)
+		if len(str) == 1024 {
+			fmt.Print("str len:", len(str), "\noutput:\n", str)
+			break
+		}
+		/*fmt.Print("str len:", len(str), "\noutput:\n", str)
+		if err != nil {
+			break
+		}*/
+	}
+	/*fmt.Println("sleeping-----------")
 	cmd2 := exec.Command("sleep", "20")
 	if err := cmd2.Run(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("slept")
-	pgid, err := syscall.Getpgid(cmd.Process.Pid)
+	fmt.Println("slept")*/
+	/*for true {
+		//fmt.Println("out:", outb.String(), "err:", errb.String())
+
+		if len(outb.String()) > 0 {
+			fmt.Println("out:", outb.String(), "err:", errb.String())
+		}
+	}
+	fmt.Println("out:", outb.String(), "err:", errb.String())*/
+	cmd.Wait()
+
+	/*pgid, err := syscall.Getpgid(cmd.Process.Pid)
 	if err == nil {
 		fmt.Println("killing the process")
 		err := syscall.Kill(-pgid, 15)
@@ -31,7 +68,8 @@ func startConv() *exec.Cmd {
 		}
 	}
 
-	cmd.Wait()
+	cmd.Wait()*/
+
 	/*err := cmd.Process.Kill()
 
 	if err != nil {
